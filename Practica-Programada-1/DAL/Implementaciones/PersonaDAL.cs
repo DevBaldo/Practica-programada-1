91f8a53f05266c3e82df7a88337e62cc5d008eba
DevBaldo <morgan1baldo@gmail.com>
2025-02-13 14:12:05 -0600

    BackEnd integrado

---
+++ Practica-Programada-1/DAL/Implementaciones/PersonaDAL.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementaciones
+{
+    public class PersonaDAL : IPersonaDAL
+    {
+        private GestionPersonasContext _context;
+
+        public PersonaDAL(GestionPersonasContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddPersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePersona(Persona persona) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Persona> GetAllPersona()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
