aee391828e1d2d5d3c1140c626424093d86981c0
DevBaldo <morgan1baldo@gmail.com>
2025-02-13 13:51:27 -0600

    Entidades

---
+++ Practica-Programada-1/Entities/Entities/GestionPersonasContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Entities;
+
+public partial class GestionPersonasContext : DbContext
+{
+    public GestionPersonasContext()
+    {
+    }
+
+    public GestionPersonasContext(DbContextOptions<GestionPersonasContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=PC; Database=GestionPersonas; Integrated Security=True; Trusted_Connection=True; Encrypt=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Persona__3214EC070CE05E4F");
+
+            entity.ToTable("Persona");
+
+            entity.HasIndex(e => e.Identificacion, "UQ__Persona__D6F931E5B494175B").IsUnique();
+
+            entity.Property(e => e.Identificacion).HasMaxLength(50);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.PrimerApellido).HasMaxLength(100);
+            entity.Property(e => e.SegundoApellido).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Practica-Programada-1/Entities/Entities/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class Persona
+{
+    public int Id { get; set; }
+
+    public string Identificacion { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string PrimerApellido { get; set; } = null!;
+
+    public string SegundoApellido { get; set; } = null!;
+}
